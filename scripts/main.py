5382a8f630f2fc788d294dcc02ca996b4feb7bec
mac yx <fsdf@f.com>
2023-05-23 15:13:50 +0800

    1

--- scripts/main.py
+++ scripts/main.py
@@ -1,14 +1,20 @@
+"""
+pyenv activate py3.11_virtualenv_test
+"""
 import os
 import io
 import json
 import gradio as gr
 
-import modules.scripts as scripts
-from modules import script_callbacks
-from modules.shared import opts
-from modules.paths import models_path
+yx_debug = True
 
-from basicsr.utils.download_util import load_file_from_url
+if not yx_debug:
+    import modules.scripts as scripts
+    from modules import script_callbacks
+    from modules.shared import opts
+    from modules.paths import models_path
+
+    from basicsr.utils.download_util import load_file_from_url
 
 
 # class Script(scripts.Script):
@@ -30,10 +36,11 @@ def welcome(name):
     # print(os.system("ls -l"))
     return f"Welcome to Gradio, {name}!"
 
+
 def on_ui_tabs():
     with gr.Blocks(analytics_enabled=False) as depth_lib_1:
         gr.Markdown(
-        """
+            """
         # Hello World!
         Start typing below to see the output.
         """)
@@ -44,12 +51,14 @@ def on_ui_tabs():
         btn = gr.Button(value="提交")
         # 绑定按钮事件
         btn.click(welcome, inputs=[inp], outputs=[out])
-
+        # 用于显示日志的html框
+        gr.HTML("fsdafsdafsdafasf<br/>fdsfdsf<br/>哈哈哈佛")
 
     return [(depth_lib_1, "Yx Test", "depth_lib_1")]  # 界面上的选项
 
 
-
-
-
-script_callbacks.on_ui_tabs(on_ui_tabs)
+if not yx_debug:
+    script_callbacks.on_ui_tabs(on_ui_tabs)
+else:
+    demo = on_ui_tabs()
+    demo[0][0].launch()
