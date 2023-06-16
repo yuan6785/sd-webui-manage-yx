04fb9f1c921b15796c5948d4ad28eb11481e5ee2
mac yx <fsdf@f.com>
2023-06-16 13:57:19 +0800

    1

--- scripts/main.py
+++ scripts/main.py
@@ -9,5 +9,5 @@ if not yx_debug:
     script_callbacks.on_ui_tabs(on_ui_tabs)
 else:
     demo = on_ui_tabs()
-    demo[0][0].queue(concurrency_count=3)  # 加这个非常重要，否则有时候会界面出现卡死的情况，无法重试
+    # demo[0][0].queue(concurrency_count=3)  # 加这个非常重要，否则有时候会界面出现卡死的情况，无法重试, webui.py里面加了，这可以不用加
     demo[0][0].launch()
