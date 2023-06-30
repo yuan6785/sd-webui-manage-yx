922b5f800c77c8b7b39a351e613680ea838e167d
mac yx <fsdf@f.com>
2023-06-30 18:08:00 +0800

    1

--- scripts/before_start.py
+++ scripts/before_start.py
@@ -22,12 +22,58 @@ def set_nas_output():
     @des: 启动时候动态设置配置项
     环境变量参考: 直接容器内读云函数环境变量 https://www.alibabacloud.com/help/zh/function-compute/latest/environment-variables
     这个文档里面还可以根据python的sdk配置环境变量，可实现动态设置环境变量，如果每个人对应的域名是不固定的，就可以按这个方式来设置。
-    >>> import os
-    >>> os.environ.get("FC_FUNCTION_NAME", "unknown")
-    'sd1'
-    >>> os.environ.get("FC_SERVICE_NAME", "unknown")
-    'machine-learning'
-    >>> 
+    ---------------------
+    通过SDK配置环境变量
+    以Python SDK为例，环境变量的参数为environmentVariables，参数取值以字典形式存储。创建、更新、获取环境变量的示例代码如下。
+
+    创建环境变量
+    # coding: utf-8
+    import fc2
+    import os
+
+    client = fc2.Client(
+        endpoint='your endpoint', # 接入点信息。具体信息，请参见https://www.alibabacloud.com/help/en/function-compute/latest/endpoints。
+        # 阿里云账号AccessKey拥有所有API的访问权限，建议您使用RAM用户进行API访问或日常运维。
+        # 建议不要把AccessKey ID和AccessKey Secret保存到工程代码里，否则可能导致AccessKey泄露，威胁您账号下所有资源的安全。
+        # 本示例以将AccessKeyID和AccessKeySecret保存在环境变量中实现身份验证为例。
+        # 运行本示例前请先在本地环境中设置环境变量ALIBABA_CLOUD_ACCESS_KEY_ID和ALIBABA_CLOUD_ACCESS_KEY_SECRET。
+        # 在FC Runtime运行环境下，配置执行权限后，ALIBABA_CLOUD_ACCESS_KEY_ID和ALIBABA_CLOUD_ACCESS_KEY_SECRET环境变量会自动被设置。
+        accessKeyID=os.getenv('ALIBABA_CLOUD_ACCESS_KEY_ID'), # AccessKey ID，阿里云身份验证，在RAM控制台创建。
+        accessKeySecret=os.getenv('ALIBABA_CLOUD_ACCESS_KEY_SECRET') # AccessKey Secret，阿里云身份验证，在RAM控制台创建。
+
+    client.create_service('test')
+
+    client.create_function(
+        'test', 'test_env', 'python3',  'main.handler',
+        codeDir='/path/to/code/', environmentVariables={'testKey': 'testValue'})
+
+    #test 服务名
+    #test_env 函数名
+    #python3  Runtime类型
+    #main.handler 请求处理程序
+    #codeDir 代码目录
+    #environmentVariables 要配置的环境变量
+
+    res = client.get_function('test', 'test_env')
+
+    print(res.data)
+    更新环境变量
+    client.update_function(
+        'test', 'test_env', 'python3',  'main.handler',
+        codeDir='/path/to/code/', environmentVariables={'newKey': 'newValue'})
+    res = client.get_function('test', 'test_env')
+    print res.data            
+    获取环境变量
+    resp = client.get_function('test', 'test_env')
+    env = func['environmentVariables']
+    在代码中使用环境变量
+    假设配置的环境变量为{"key":"val"}，以下为各运行环境读取并打印此环境变量值的方法。
+
+    Node.jsPythonJavaPHPC
+
+    import os
+    value = os.environ.get('key')
+    print(value)
     """
     # 从这里/Users/yuanxiao/workspace/0yxgithub/userful_scripts/project_pre/yxpre/pre_make.py生成拷贝过来---重要---
     domain_map_name = {"6": "chenmin", "7": "duanyi", "8": "huangzhipeng", "9": "lihai", "10": "lvxing", "11": "mahaiteng", "12": "wangjiaxin", "13": "zhaoyue", "14": "wangluo", "15": "sunfuxing", "16": "daixinxin", "17": "yangguoqing", "18": "zhaozilong", "19": "jiaokun", "20": "lijie", "21": "liyonggang", "22": "panzhonghao", "23": "yuganfeng", "24": "zhaolili", "25": "wangxiaoqing", "26": "zhanghongzhi", "27": "hehuaying", "28": "jixiaomin", "29": "lvshanshan",
